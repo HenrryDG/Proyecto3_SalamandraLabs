c087b8c23095c48153607cf80f4512ba1c54c913
Henrry <148930610+HenrryDG@users.noreply.github.com>
2025-09-26 17:46:23 -0400

    agregar servicio de autenticación y manejo automático para refrescar el  access token en caso de que una solicitud a la API falle en Axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { refreshAccessToken, logout } from "./auth/authService"
 
 
 const api = axios.create({
@@ -15,5 +16,30 @@ api.interceptors.request.use((config) => {
     }
     return config
 })
+
+// INTERCEPTOR PARA MANEJAR ERRORES DE AUTENTICACIÓN (401) Y REFRESCAR EL TOKEN
+api.interceptors.response.use(
+    response => response,
+    async error =>{
+        const originalRequest = error.config;
+
+        if(error.response.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true;
+
+            const newAccessToken = await refreshAccessToken(); 
+
+            if (newAccessToken){
+                // Actualizar el header y reintentar la petición
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                return api(originalRequest);
+            }
+            else{
+                // Si no se puede refrescar el token, cerrar sesión
+                await logout();
+            }
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default api
