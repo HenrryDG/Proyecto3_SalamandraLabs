e1e2302447290eb2e4e7723728a7e7c0b901a67b
Maho Salinas <majitosp2002@gmail.com>
2025-10-12 22:44:27 -0400

    Prueba de tabla para empleados

---
+++ src/components/tables/empleado/EmpleadoTable.tsx
@@ -0,0 +1,94 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHeader,
+    TableRow,
+} from "../../ui/table";
+import Button from "../../ui/button/Button";
+import Badge from "../../ui/badge/Badge";
+import { MoreDotIcon } from "../../../icons";
+import { Empleado } from "../../../types/empleado";
+
+type Props = {
+    empleados: Empleado[];
+    onEdit: (empleado: Empleado) => void;
+};
+
+export default function EmpleadoTable({ empleados, onEdit }: Props) {
+    return (
+        <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+            <div className="max-w-full overflow-x-auto">
+                <Table>
+                    <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+                        <TableRow>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Empleado</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">CI</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Dirección</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Teléfono</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Correo</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Cargo</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Estado</TableCell>
+                            <TableCell isHeader className="px-5 py-3 text-start text-theme-xs font-medium text-gray-500 dark:text-gray-400">Acciones</TableCell>
+                        </TableRow>
+                    </TableHeader>
+
+                    <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+                        {empleados.map((empleado) => (
+                            <TableRow key={empleado.id}>
+                                <TableCell className="px-5 py-4 sm:px-6 text-start">
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                                {empleado.nombre} {empleado.apellido_paterno} {empleado.apellido_materno}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.user}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.nombre}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.apellido_paterno}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.apellido_materno}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.correo ? empleado.correo : "-"}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.telefono}
+                                </TableCell>
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    {empleado.rol}
+                                </TableCell>                                
+                                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                                    <Badge
+                                        size="sm"
+                                        color={empleado.activo ? "success" : "warning"}
+                                    >
+                                        {empleado.activo ? "Activo" : "Inactivo"}
+                                    </Badge>
+                                </TableCell>
+                                <TableCell className="px-4 py-3">
+                                    <Button
+                                        variant="outline"
+                                        size="md"
+                                        endIcon={<MoreDotIcon className="size-5" />}
+                                        onClick={() => onEdit(empleado)}
+                                    >
+                                        {" "}
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    );
+}
