27c6f5d27af53646e1a72c7e252e76aaab0700ea
Mah0j0 <spm2028248@est.univalle.edu>
2025-10-17 23:50:39 -0400

    validación de contraseña y coreo en la configuración

--- src/components/form/configs/empleadoFormConfig.ts
+++ src/components/form/configs/empleadoFormConfig.ts
@@ -2,6 +2,8 @@ import {
     validarTexto,
     validarTelefono,
     validarCorreo,
+    validarCorreoProveedor,
+    validarContrasena,
     validarLongitud,
     validarTextoMinimo
 } from "../../utils/validaciones";
@@ -22,11 +24,11 @@ export const campos: {
         { key: "nombre", label: "Nombre", validator: (v) => validarTextoMinimo(v, 3) || validarTexto(v) },
         { key: "apellido_paterno", label: "Apellido Paterno", validator: (v) => validarTextoMinimo(v, 3) || validarTexto(v) },
         { key: "apellido_materno", label: "Apellido Materno", validator: (v) => validarTextoMinimo(v, 3) || validarTexto(v) },
-        { key: "correo", label: "Correo", type: "email", validator: (v) => !v ? null : validarLongitud(v, 1, 50) || validarCorreo(v) },
+        { key: "correo", label: "Correo", type: "email", validator: (v) => !v ? null : validarLongitud(v, 1, 50) || validarCorreo(v) || validarCorreoProveedor(v) },
         { key: "telefono", label: "Teléfono", validator: validarTelefono },
         { key: "rol", label: "Rol", validator: (v) => validarTextoMinimo(v, 3) || validarTexto(v) },
         { key: "username", label: "Usuario", validator: (v) => validarLongitud(v, 3, 20) },
-        { key: "password", label: "Contraseña", type: "password", validator: (v) => validarLongitud(v, 4, 100) || validarTexto(v) }
+        { key: "password", label: "Contraseña", type: "password", validator: (v) => validarContrasena(v, 8) }
     ];
 
 export const maxLengths: Record<FormKeys, number> = {
