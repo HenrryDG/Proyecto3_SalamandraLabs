f10a6f87ec2646b511f5f552af67e6a6ba9c2b74
gabriel-arinez <gabrielarinez056@gmail.com>
2025-11-03 18:49:38 -0400

    Creación del tipo para Planes de pago

---
+++ src/types/planPago.ts
@@ -0,0 +1,20 @@
+export type PlanPagoEstado = 'Pendiente' | 'Pagada' | 'Vencida';
+export type MetodoPago = 'QR' | 'Efectivo';
+
+export interface PlanPago {
+    id: number;
+    prestamo: number;
+    fecha_pago: string | null;
+    fecha_vencimiento: string;
+    metodo_pago: MetodoPago | null;
+    monto_cuota: string;
+    mora_cuota: string;
+    estado: PlanPagoEstado;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface PlanPagoActualizacion {
+    metodo_pago?: MetodoPago;
+    estado?: PlanPagoEstado;
+}
